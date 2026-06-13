e320e00e375c9cf5a9da70c35497b1983fb6b893
Ruona <ruonao5@gmail.com>
2026-06-13 12:16:36 +0100

    Added the database connection adn the tester code to the inital page to see current database

---
+++ lib/db/db.ts
@@ -0,0 +1,18 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { awsCredentialsProvider } from "@vercel/functions/oidc";
+
+const roleArn = process.env.AWS_ROLE_ARN;
+if (!roleArn) {
+  throw new Error("AWS_ROLE_ARN is required");
+}
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+  credentials: awsCredentialsProvider({
+    roleArn,
+    clientConfig: { region: process.env.AWS_REGION },
+  }),
+});
+
+export const docClient = DynamoDBDocumentClient.from(client);
